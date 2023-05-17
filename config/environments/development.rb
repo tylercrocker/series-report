a277ec9ae11585bf1f2a24cd109e208954b9fd2b
Tyler Crocker <crocker.tylerj@gmail.com>
2023-05-17 13:15:11 -0700

    resetting dev env defaults

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,10 +6,10 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Do not eager load code on boot.
-  config.eager_load = true
+  config.eager_load = false
 
   # Show full error reports.
   config.consider_all_requests_local = true
@@ -49,8 +49,8 @@ Rails.application.configure do
   config.active_record.migration_error = :page_load
 
   # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = false # true
-  config.log_level = :info # comment out
+  config.active_record.verbose_query_logs = true # true
+  # config.log_level = :info # comment out
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
